b1b834945e97e6f78ca441fb8ce48e52f2bbe081
Cow Duck <madduckoff@gmail.com>
2023-02-07 06:47:53 +0300

    first commit

---
+++ src/components/BattleView.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Battle"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/MonsterView.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Monster"
+}
+</script>
+
+<style scoped>
+
+</style>
